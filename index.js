15ff43a7093fc406606fb17183081b0e481e23ce
Benjamin Turner <benjamin@passionsplay.com>
2024-04-02 22:04:01 -0700

    feat: add watch functionality

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 #!/usr/bin/env node
 const path = require('path');
+const chokidar = require('chokidar');
 const puppeteer = require('puppeteer');
 
 const argv = require('yargs/yargs')(process.argv.slice(2))
     .usage('Usage: $0 <html-file>')
     .example('$0 index.html', 'Convert index.html to index.pdf')
+    .example('$0 index.html -w', 'Watch for changes to index.html and regenerate index.pdf')
     .demandCommand(1)
+    .option('watch', {
+        alias: 'w',
+        describe: 'Watch the html file for changes and regenerate the pdf',
+        type: 'boolean'
+    })
     .parse();
 
 const htmlFile = argv._[0];
+const shouldWatch = argv.watch;
 
 (async function () {
 	try {
-		await generatePdf(htmlFile);
+        if (shouldWatch) {
+            chokidar.watch(htmlFile)
+                .on('ready', () => console.log(`Watching for changes to ${htmlFile}...`))
+                .on('add', generatePdf)
+                .on('change', (htmlFile) => {
+                    console.log('File changed. Regenerating pdf...');
+                    generatePdf(htmlFile);
+                })
+                .on('unlink', () => {
+                    console.log('File removed. Stopping watching...');
+                    process.exit(1);
+                });
+        } else {
+            await generatePdf(htmlFile);
+            process.exit(0);
+        }
 	} catch (e) {
 		console.log(e);
+        process.exit(1);
 	}
 })();
 
