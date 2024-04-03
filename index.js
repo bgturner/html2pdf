f590a751931c00fd5aea347f795ddee0d9aeeeb7
Benjamin Turner <benjamin@passionsplay.com>
2024-04-02 21:32:46 -0700

    feat: generate pdf from passed html filename

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
-console.log("Hello")
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const htmlFile = process.argv[2];
+
+(async function () {
+	try {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+
+        const filePath = path.join(__dirname, htmlFile);
+        await page.goto(`file://${filePath}`);
+
+        const pdfFilePath = getPdfName(filePath);
+
+		await page.pdf({ path: pdfFilePath, format: 'letter' });
+
+		await browser.close();
+	} catch (e) {
+		console.log(e);
+	}
+})();
+
+function getPdfName(htmlFile) {
+    const basename = path.basename(htmlFile, '.html');
+    return path.join(__dirname, `${basename}.pdf`);
+}
