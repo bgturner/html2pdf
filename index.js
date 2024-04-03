63d39ca9aa1de98c3aa8dfbe16e42569c3d36a3b
Benjamin Turner <benjamin@passionsplay.com>
2024-04-02 21:44:59 -0700

    feat: use yargs to process positional args

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const path = require('path');
 const puppeteer = require('puppeteer');
 
-const htmlFile = process.argv[2];
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage('Usage: $0 <html-file>')
+    .example('$0 index.html', 'Convert index.html to index.pdf')
+    .demandCommand(1)
+    .parse();
+
+const htmlFile = argv._[0];
 
 (async function () {
 	try {
